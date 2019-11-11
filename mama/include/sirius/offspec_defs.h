57060eedaf2bb0ff29f3f8b0f45fb02543565d62
Vetle Wegner Ingeberg <vetlewi@fys.uio.no>
2019-11-11 14:28:02 +0100

    Changed build system to CMake

---
+++ mama/include/sirius/offspec_defs.h
@@ -0,0 +1,26 @@
+#define  SINGLES_KEY		5500     /* Singles spectra S1-S10 	(4096) 		*/
+#define  ESP_KEY			5501     /* E-counter spectra  		(2048x64)   */
+#define  DESP_KEY			5502     /* DE-counter spectra 		(2048x64)  	*/
+#define  EDESP_KEY			5503     /* DE-E-counter spectra 	(2048x64) 	*/
+#define  THICKSP_KEY		5504     /* Thickness spectra 		(2048x64) 	*/
+#define  GESP_KEY			5505	 /* Ge spectra				(4096x6)	*/
+#define  TGESP_KEY			5506	 /* Ge T spectra			(512x6)		*/
+#define  NASP_KEY			5507	 /* NaI spectra				(2048x32)	*/
+#define  TNASP_KEY			5508	 /* NaI T spectra			(512x32)	*/
+#define  ALFNA_KEY			5509	 /* Alpha-NaI matrix		(2048x512)	*/
+#define  ALFGE_KEY			5510	 /* Alpha-Ge matrix			(2048x512)	*/
+#define  MAT_KEY			5511	 /* General purpose matrix	(2048x64)	*/
+
+
+#define  SINGLES_SIZE		163840   /* 160 kB */
+#define	 ESP_SIZE           524288   /* 512 kB */
+#define	 DESP_SIZE          524288   /* 512 kB */
+#define	 EDESP_SIZE         524288   /* 512 kB */
+#define	 THICKSP_SIZE       524288   /* 512 kB */
+#define	 GESP_SIZE          98304    /* 96 kB  */
+#define	 TGESP_SIZE         12288    /* 12 kB  */
+#define  NASP_SIZE          262144   /* 256 kB */
+#define  TNASP_SIZE			65536 	 /* 64 kB  */
+#define  ALFNA_SIZE			4194304	 /* 4 MB   */
+#define	 ALFGE_SIZE			4194304	 /* 4 MB   */
+#define	 MAT_SIZE			524288   /* 512 kB */
