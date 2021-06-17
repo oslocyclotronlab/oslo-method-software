98c97c9ede97477793acd703f157f7d988438815
magneg <magne.guttormsen@fys.uio.no>
2021-06-17 12:31:20 +0200

    Delete sirius directory
    Not more in use

--- sirius/include/redmem_defs.h
+++
@@ -1,3 +0,0 @@
-#define  PERMS				0666	/* ReadWrite for all */
-#define  SHAREDMEM_KEY		6000	/* Identifies shared memory set */
-#define  MESSAGE_KEY		6100	/* Shared memory segment for message box */
--- sirius/offline/src/getbits.c
+++
@@ -1,7 +0,0 @@
-#include	<stdio.h>
-#include	<stdlib.h>
-
-unsigned getbits(unsigned x, int p, int n)
-{
-	return ( x >> (p + 1 - n)) & ~(~0 << n);
-}
